cc9321eb425a469cf9357284a309653a764762a7
June-Ruth <n-barral@hotmail.fr>
2024-01-02 15:46:24 +0100

    feat(backend): manage default success irl when authentification success

--- MedHeadBackEnd/src/main/java/com/medhead/emergency/configuration/SpringSecurityConfiguration.java
+++ MedHeadBackEnd/src/main/java/com/medhead/emergency/configuration/SpringSecurityConfiguration.java
@@ -4,15 +4,10 @@ import com.medhead.emergency.service.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,11 +22,19 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/admin").hasRole("ADMIN");
-            auth.requestMatchers("/user").hasRole("USER");
-            auth.anyRequest().authenticated();
-                }).formLogin(Customizer.withDefaults()).build();
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/admin").hasRole("ADMIN");
+                    auth.requestMatchers("/user").hasRole("USER");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(form ->
+                        form.loginProcessingUrl("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/user")
+                );
+
+        return http.build();
     }
 
     @Bean
@@ -40,21 +43,6 @@ public class SpringSecurityConfiguration {
         authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
         return authenticationManagerBuilder.build();
     }
-
-   /* @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    } */
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
